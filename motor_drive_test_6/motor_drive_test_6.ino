f580723102bbb8b85b7f0c9dc6ea0138038b3c46
Sean Purser-Haskell <sean.purserhaskell@gmail.com>
2015-09-04 10:27:41 +0800

    Variable speed, beginning pwm

--- motor_drive_test_6/motor_drive_test_6.ino
+++ motor_drive_test_6/motor_drive_test_6.ino
@@ -25,6 +25,9 @@ void setup() {
   for(int i=0;i<2;++i) {
     go_dead(i);
   }
+
+  Serial.begin(9600);
+  Serial.println("------- Rebooted -----");
 }
 
 const int winding_quarters_forward[2][4] = {
@@ -77,12 +80,121 @@ void run_const_speed(const int winding_quarters[2][4],
   go_dead_all();
 }
 
+#if 1
 void loop() {
-  const unsigned long cycle_micros = 20000;
-  static boolean f = true;
+  const int winding = 0;
+  go_dead(winding);
+  delayMicroseconds(50);
+  
+  const unsigned long half_pulse_micros = 250;
+  
+  for(unsigned long ml_s = millis();(millis() - ml_s) < 10;ml_s) {
+    digitalWrite(pullups_positive[winding], HIGH);
+    digitalWrite(pulldowns_positive[winding], LOW);
+    delayMicroseconds(half_pulse_micros);
+    digitalWrite(pullups_positive[winding], LOW);
+    digitalWrite(pulldowns_positive[winding], LOW);
+    delayMicroseconds(5);
+    digitalWrite(pullups_positive[winding], LOW);
+    digitalWrite(pulldowns_positive[winding], HIGH);
+    delayMicroseconds(half_pulse_micros);
+  }
+  
+  /*
+  digitalWrite(pullups_positive[winding], HIGH);
+  digitalWrite(pulldowns_positive[winding], LOW);
+  delay(10);
+  */
+  
+  go_dead(winding);
+  delayMicroseconds(50);
+  
+  
+  for(unsigned long ml_s = millis();(millis() - ml_s) < 10;ml_s) {
+    digitalWrite(pullups_negative[winding], HIGH);
+    digitalWrite(pulldowns_negative[winding], LOW);
+    delayMicroseconds(half_pulse_micros);
+    digitalWrite(pullups_negative[winding], LOW);
+    digitalWrite(pulldowns_negative[winding], LOW);
+    delayMicroseconds(5);
+    digitalWrite(pullups_negative[winding], LOW);
+    digitalWrite(pulldowns_negative[winding], HIGH);
+    delayMicroseconds(half_pulse_micros);
+  }
+  /*
+  digitalWrite(pullups_negative[winding], HIGH);
+  digitalWrite(pulldowns_negative[winding], LOW);
+  delay(10);
+  */
+  
+}
+#endif
+
+#if 0
+// Continuous variable speed test
+void loop() {
+  const unsigned long cycle_micros_min = 12500;
+  const unsigned long cycle_micros_max = 22000;
+  const unsigned long cycle_micros = cycle_micros_min + 
+      int((cycle_micros_max - cycle_micros_min) * (0.5f + 0.5f * sin((millis()%10000) * (6.28f/10000.0f))));
+  const unsigned long quarter_micros = cycle_micros / 4;
+  // Need to reset each time to ensure forward/reverse
+  static int quarter = 0;
+  for(int winding=0;winding<2;++winding) {
+    if(winding_quarters_forward[winding][quarter] !=
+       winding_quarters_forward[winding][quarter_before(quarter)]) {
+      go_dead(winding);
+      delayMicroseconds(50);
+      if(winding_quarters_forward[winding][quarter]) {
+        digitalWrite(pullups_negative[winding], LOW);
+        digitalWrite(pulldowns_negative[winding], HIGH);
+        
+        digitalWrite(pullups_positive[winding], HIGH);
+        digitalWrite(pulldowns_positive[winding], LOW);
+      } else {
+        digitalWrite(pullups_positive[winding], LOW);
+        digitalWrite(pulldowns_positive[winding], HIGH);
+        
+        digitalWrite(pullups_negative[winding], HIGH);
+        digitalWrite(pulldowns_negative[winding], LOW);
+      }
+    }
+  }
+  delayMicroseconds(quarter_micros);
+  quarter = (quarter+1) % 4;
+}
+#endif
+
+#if 0
+// Basic test
+void loop() {
+  static int test_idx = 0;
+  boolean f = (test_idx % 2) == 0;
+  const unsigned long cycle_micros = 1000000L / (50 + 10 * ((test_idx)/2));
+
+  Serial.print("Beginning test: ");
+  if(f) {
+    Serial.print("forward ");
+  } else {
+    Serial.print("reverse ");
+  }
+  Serial.print("cycle micros (");
+  Serial.print(cycle_micros);
+  Serial.println(")");
+
   run_const_speed(f ? winding_quarters_forward : winding_quarters_reverse,
                   cycle_micros,
-                  2000);
-  f = !f;
-  delay(2000);
+                  1500);
+  test_idx = (test_idx+1) % 8;
+  delay(1000);
 }
+#endif
+
+#if 0
+void loop() {
+  run_const_speed(winding_quarters_forward,
+                  20000,
+                  100000);
+}
+#endif
+
